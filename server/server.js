239fa623ecc0f13b4f2fa038b34f407c67fbcee4
Pranjal Maithani <pranjalmaithani81822@gmail.com>
2025-08-24 18:24:12 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const clientRoutes = require('./routes/client');
 const { MONGODB_URI } = require('./config/db'); // your config file exporting the URI
 
 
-const app = express();
+const app = express.json();
 
 // Connect to MongoDB
 mongoose.connect(MONGODB_URI)
@@ -24,14 +24,15 @@ mongoose.connect(MONGODB_URI)
       credentials: true,
     }));
 
-    // Routes
-    app.use('/',(req,res)=>{
-      res.send('server is running');
-    })
+    
     app.use('/api/admin', adminRoutes);
     app.use('/api/auth', authRoutes);
     app.use('/api/client', clientRoutes);
     app.use('/api/employee', employeeRoutes); 
+    // Routes
+    app.use('/',(req,res)=>{
+      res.send('server is running');
+    })
     // 404 handler
     app.use((req, res, next) => {
       res.status(404).json({ status: 'fail', message: 'Route not found' });
